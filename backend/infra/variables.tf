3536b0c54a9bad0af31fffe64d871780813e2347
John Pratt <john@john-pratt.com>
2025-08-13 20:11:17 -0400

    simplify secrets, add new one, add lambda layer for requests.py

--- backend/infra/variables.tf
+++ backend/infra/variables.tf
@@ -8,12 +8,23 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# # variables.tf
-# variable "db_password" {
-#   type      = string
-#   sensitive = true
-# }
-# variable "api_token" {
-#   type      = string
-#   sensitive = true
-# }
+variable "github_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "article_blacklist_strings" {
+    type      = string
+    sensitive = true
+}
+
+variable "external_url_regex" {
+    type      = string
+    sensitive = true
+}
+
+variable "python_runtime" {
+  default = "python3.11"
+}
+
+# variable
