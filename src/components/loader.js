1abe229343b0b6bd407a84ba6823e76914cc3e4d
John Pratt <john@john-pratt.com>
2025-10-04 15:30:22 -0400

    upgrade to animejs 4.2.0, add placeholder test script

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
-import anime from 'animejs';
+import { createTimeline, svg } from 'animejs';
 import styled from 'styled-components';
 import { IconLoader } from '@components/icons';
 
@@ -39,45 +39,50 @@ const StyledLoader = styled.div`
 const Loader = ({ finishLoading }) => {
   const [isMounted, setIsMounted] = useState(false);
 
-  const animate = () => {
-    const loader = anime.timeline({
-      complete: () => finishLoading(),
+  const animateLogo = () => {
+    const loader = createTimeline({
+      onComplete: () => finishLoading(), // v4 timeline callback name
     });
 
-    loader
-      .add({
-        targets: '#logo path',
+    // 1) Draw the logo paths using the new drawable + `draw` property
+    loader.add(
+      svg.createDrawable('#logo path'), // returns drawable proxies
+      {
         delay: 300,
         duration: 1500,
-        easing: 'easeInOutQuart',
-        strokeDashoffset: [anime.setDashoffset, 0],
-      })
-      .add({
-        targets: '#logo #B',
-        duration: 700,
-        easing: 'easeInOutQuart',
-        opacity: 1,
-      })
-      .add({
-        targets: '#logo',
-        delay: 500,
-        duration: 300,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        scale: 0.1,
-      })
-      .add({
-        targets: '.loader',
-        duration: 200,
-        easing: 'easeInOutQuart',
-        opacity: 0,
-        zIndex: -1,
-      });
+        ease: 'inOutQuart',
+        draw: '0 1', // replaces strokeDashoffset + setDashoffset
+      }
+    );
+
+    // 2) Fade in the #B part
+    loader.add('#logo #B', {
+      duration: 700,
+      ease: 'inOutQuart',
+      opacity: 1,
+    });
+
+    // 3) Fade/scale out the whole logo
+    loader.add('#logo', {
+      delay: 500,
+      duration: 300,
+      ease: 'inOutQuart',
+      opacity: 0,
+      scale: 0.1,
+    });
+
+    // 4) Hide the loader container
+    loader.add('.loader', {
+      duration: 200,
+      ease: 'inOutQuart',
+      opacity: 0,
+      zIndex: -1,
+    });
   };
 
   useEffect(() => {
     const timeout = setTimeout(() => setIsMounted(true), 10);
-    animate();
+    animateLogo();
     return () => clearTimeout(timeout);
   }, []);
 
