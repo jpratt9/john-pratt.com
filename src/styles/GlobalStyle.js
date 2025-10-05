c1bc7326a3ffeca01b05fb96f3b283d8a1a6bb8b
John Pratt <john@john-pratt.com>
2025-10-04 20:22:37 -0400

    fix PrismStyles import

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 import fonts from './fonts';
 import variables from './variables';
 import TransitionStyles from './TransitionStyles';
-// import PrismStyles from './PrismStyles';
+import PrismStyles from './PrismStyles';
 
 const GlobalStyle = createGlobalStyle`
   ${fonts};
@@ -446,7 +446,6 @@ const GlobalStyle = createGlobalStyle`
 
   ${TransitionStyles};
 
-  ${PrismStyles};
 `;
 
 export default GlobalStyle;
